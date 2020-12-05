ffe708ab20c0e330cb836837054cc6c9d0ac28eb
elenavondermeulen <elenavonder20@outlook.com>
2020-12-04 20:02:28 -0500

    * Vendors.cs: * Products.cs:
    * Guest.cs: Added new classes

---
+++ Models/Products.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WeddingProject2.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string PartNbr { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+        [Column(TypeName = "decimal (11,2)")]
+        public decimal Price { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Unit { get; set; } = "each";
+        [StringLength(255)]
+        public string PhotoPath { get; set; }
+        public int VendorId { get; set; }
+        public virtual Vendors Vendor { get; set; }
+
+        public Products()
+        {
+        }
+    }
+}
--- Models/Vendors.cs
+++ Models/Vendors.cs
@@ -3,27 +3,33 @@ using System.ComponentModel.DataAnnotations;
 
 namespace WeddingProject2.Models
 {
-    public class Guest
+    public class Vendors
     {
         public int Id { get; set; }
         [Required]
         [StringLength(30)]
-        public string Username { get; set; }
+        public string Code { get; set; }
         [Required]
         [StringLength(30)]
-        public string Password { get; set; }
+        public string Name { get; set; }
         [Required]
         [StringLength(30)]
-        public string Firstname { get; set; }
+        public string Address { get; set; }
         [Required]
         [StringLength(30)]
-        public string Lastname { get; set; }
+        public string City { get; set; }
+        [Required]
+        [StringLength(2)]
+        public string State { get; set; }
+        [Required]
+        [StringLength(5)]
+        public string Zip { get; set; }
         [StringLength(12)]
         public string Phone { get; set; }
         [StringLength(255)]
         public string Email { get; set; }
-
-        public Guest()
+        
+        public Vendors()
         {
         }
     }
